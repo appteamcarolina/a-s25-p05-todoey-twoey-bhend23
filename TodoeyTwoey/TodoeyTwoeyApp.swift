bb818f2e509d28cc73a17152d541b10cb3ebc357
bhend23 <bhend@ad.unc.edu>
2025-08-17 12:49:06 -0400

    maybe fix

---
+++ TodoeyTwoey/TodoeyTwoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyTwoeyApp.swift
+//  TodoeyTwoey
+//
+//  Created by Brandon Henderson on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyTwoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
